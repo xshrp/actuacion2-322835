973a9565fc1f458e9274c9f8ac035b95e4ba2bf0
GonzaOrtellado <200260358+xshrp@users.noreply.github.com>
2026-05-21 20:31:18 -0300

    fix: stop de apache

--- user_data.sh
+++ user_data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 yum install -y httpd
 systemctl enable httpd
-systemctl stop httpd
 
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
   -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
