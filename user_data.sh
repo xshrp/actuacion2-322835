a3e66901612d3e7f2f7db4164f3ef5cde7b74a91
GonzaOrtellado <200260358+xshrp@users.noreply.github.com>
2026-05-21 20:57:25 -0300

    fix: EOF

--- user_data.sh
+++ user_data.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-sudo yum install -y httpd
-sudo systemctl enable httpd
-sudo systemctl start httpd
+
+yum install -y httpd
+
+systemctl enable httpd
+systemctl start httpd
 
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
   -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
 INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
   http://169.254.169.254/latest/meta-data/instance-id)
+
 HOSTNAME=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
   http://169.254.169.254/latest/meta-data/local-hostname)
 
-cat > /var/www/html/index.html << ENDOFHTML
+cat > /var/www/html/index.html <<EOF
 <html>
 <body style="font-family: Arial; text-align: center; padding: 50px;">
   <h1>EC2 Web Server</h1>
@@ -18,4 +22,4 @@ cat > /var/www/html/index.html << ENDOFHTML
   <p><strong>Hostname:</strong> $HOSTNAME</p>
 </body>
 </html>
-ENDOFHTML
+EOF
