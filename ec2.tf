6c70142c6f604771ed2ba8e255f95145c5efd7ce
GonzaOrtellado <200260358+xshrp@users.noreply.github.com>
2026-05-21 21:13:21 -0300

    fix: desired capaciry cambiada a dos de nuevo

--- ec2.tf
+++ ec2.tf
@@ -22,7 +22,7 @@ resource "aws_launch_template" "AC2-lt" {
 
 resource "aws_autoscaling_group" "AC2-asg" {
   name                = "AC2-asg"
-  desired_capacity    = 0
+  desired_capacity    = 2
   min_size            = 2
   max_size            = 2
   vpc_zone_identifier = [aws_subnet.AC2-subnet-private-a.id, aws_subnet.AC2-subnet-private-b.id]
